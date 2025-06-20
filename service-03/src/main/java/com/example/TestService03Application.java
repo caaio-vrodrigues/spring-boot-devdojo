3a8c3bde81f2d2459723b0039c1cc711ce0f3cce
Caio <caio.vr12@gmail.com>
2025-06-19 23:40:34 -0300

    testando...

---
+++ service-03/src/main/java/com/example/TestService03Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class TestService03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestService03Application.class, args);
+	}
+
+}
